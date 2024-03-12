db4003da00d83c3634c694cc6ead79090c00f9c7
antonio.trrsch <antonio1959988@hotmail.com>
2024-03-12 10:33:38 -0600

    deployv3

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,9 @@ import asistencia from './asistencia';
 import eliminarComentario from './eliminarComentario';
 
 // obtener valores de la base de datos
-
 const lat = document.querySelector('#lat').value ||  20.666332695977;
 const lng = document.querySelector('#lng').value ||  -103.3921777456999;
-const direccion  = document.querySelector('#direccion').value || '';
+const direccion = document.querySelector('#direccion').value || ''
 const map = L.map('mapa').setView([lat, lng], 15);
 
 let markers = new L.FeatureGroup().addTo(map);
@@ -15,36 +14,35 @@ let marker;
 // Utilizar el provider y GeoCoder
 const geocodeService = L.esri.Geocoding.geocodeService();
 
-// Colocar el Pin en Edición
-
-if(lat && lng ){
-    // agregar el pin
-    marker = new L.marker([lat, lng], {
-        draggable : true,
+// Colocar el pin en edicion
+if(lat && lng && direccion){
+    // Agregar el pin
+    marker = new L.marker([lat,lng], {
+        draggable: true,
         autoPan: true
     })
     .addTo(map)
     .bindPopup(direccion)
-    .openPopup();
+    .openPopup()
 
-    // asignar al contenedor markers
-    markers.addLayer(marker);
+    // Asignar al contenedor markers
+    markers.addLayer(marker)
 
-    // detectar movimiento del marker
-    marker.on('moveend', function(e) {
-        marker = e.target;
-        const posicion = marker.getLatLng();
-        map.panTo(new L.LatLng(posicion.lat, posicion.lng) );
+                    // detectar movimiento del marker
+                    marker.on('moveend', function(e) {
+                        marker = e.target;
+                        const posicion = marker.getLatLng();
+                        map.panTo(new L.LatLng(posicion.lat, posicion.lng) );
 
-        // reverse geocoding, cuando el usuario reubica el pin
-        geocodeService.reverse().latlng(posicion, 15 ).run(function(error, result) {
+                        // reverse geocoding, cuando el usuario reubica el pin
+                        geocodeService.reverse().latlng(posicion, 15 ).run(function(error, result) {
 
-            llenarInputs(result);
-        
-            // asigna los valores al popup del marker
-            marker.bindPopup(result.address.LongLabel);
-        });
-    })
+                            llenarInputs(result);
+
+                            // asigna los valores al popup del marker
+                            marker.bindPopup(result.address.LongLabel);
+                        });
+                    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -69,7 +67,7 @@ function buscarDireccion(e) {
 
             geocodeService.reverse().latlng(resultado[0].bounds[0], 15 ).run(function(error, result) {
                 llenarInputs(result);
-                
+
                 // console.log(resultado);
                 // mostrar el mapa
                 map.setView(resultado[0].bounds[0], 15);
@@ -97,7 +95,7 @@ function buscarDireccion(e) {
                     geocodeService.reverse().latlng(posicion, 15 ).run(function(error, result) {
 
                         llenarInputs(result);
-                    
+
                         // asigna los valores al popup del marker
                         marker.bindPopup(result.address.LongLabel);
                     });
