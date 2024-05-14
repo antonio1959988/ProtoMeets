dc8e7c083e4b7298af8f1b14ec42dfb9abbdf9f9
antonio.trrsch <antonio1959988@hotmail.com>
2024-05-14 13:36:07 -0600

    config

--- config/db.js
+++ config/db.js
@@ -5,6 +5,13 @@ module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, proce
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     pool: {
         max: 5,
         min: 0,
